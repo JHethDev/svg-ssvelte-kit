b5accbed8e5d28a5eb167f35b38831f3f6b9bb07
Jason Hetherington <jason.h.hetherington@gmail.com>
2021-05-02 11:57:06 -0700

    Add REPL code

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,26 @@
+<script>
+	import Circle from '$lib/Circle.svelte'
+	import Rect from '$lib/Rect.svelte'
+	
+	let selected = Circle
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<svelte:component this={selected} />
+
+<button on:click={()=> selected = Rect}>
+	Make it square
+</button>
+<button on:click={()=> selected = Circle}>
+	Make it circle
+</button>
+
+<style>
+	:global(svg) {
+		max-width: 400px;
+		display: block
+	}
+</style>
+
